051fdb7c9ac005188c7fe0876c84a7f85464abea
Riley Rice <riceriley59@gmail.com>
2025-01-30 18:26:08 -0800

    Added super simple admin-page landing with navbar

--- fuel-kiosk/src/app/admin/page.js
+++ fuel-kiosk/src/app/admin/page.js
@@ -1,11 +1,41 @@
 'use client'
 
 import classes from "./admin.module.css";
+import { NavbarSimple } from "./components/navbar/NavbarSimple";
 
 export default function Admin() {
     return (
-        <div>
-            <p>Admin Page</p>
+        <div className={classes.container}>
+            <aside className={classes.sidebar}>
+                <NavbarSimple />
+            </aside>
+            <main className={classes.mainContent}>
+                {/* Add your main admin page content here */}
+                <h1>Welcome to the Admin Panel</h1>
+                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Volutpat torquent maximus platea tincidunt aliquam inceptos curabitur donec. Eget faucibus congue hendrerit eros enim sit vel. Pellentesque dui lectus vulputate phasellus mollis faucibus, amet pellentesque. Mauris parturient dolor quisque orci volutpat sem laoreet. Quis orci ornare cubilia amet nec. Tristique porttitor dictum in vivamus placerat integer ac. Viverra sed faucibus leo dignissim, amet tincidunt cursus viverra. Nunc porttitor ex laoreet sem molestie platea varius enim fringilla.
+
+Venenatis et amet molestie ad platea interdum. Inceptos nec ligula diam orci rhoncus. Interdum leo rhoncus in nibh venenatis ex proin dictumst. Fames mi bibendum inceptos at mattis. Sollicitudin iaculis ex sapien suspendisse metus fusce est vestibulum. Class et per sapien hendrerit facilisi habitant lacus odio. Arcu dui litora luctus nec torquent ut commodo facilisi litora. Sagittis luctus lacinia malesuada mi consectetur; ipsum velit.
+
+Torquent massa egestas malesuada vulputate accumsan. Integer fringilla sollicitudin rutrum molestie neque enim. Aliquam venenatis fringilla elit vestibulum tellus. Torquent per leo blandit porta turpis. Efficitur metus adipiscing suscipit egestas cursus. Fusce libero magna justo vel ultricies.
+
+Parturient pretium luctus posuere; rutrum id sit. Platea fringilla primis aptent orci adipiscing nam; integer mollis scelerisque. Eget libero nec ligula gravida amet et pulvinar mollis. Euismod ad suspendisse egestas molestie nisi ridiculus maecenas per. Fringilla aenean primis et montes lobortis ipsum. Malesuada iaculis magna feugiat turpis tortor semper suscipit suscipit. Quam diam cubilia sed primis sollicitudin mi sapien quam. Fringilla ut curae porttitor nunc pellentesque pellentesque pulvinar vulputate.
+
+Leo sodales magnis cubilia arcu euismod fringilla efficitur ad. Lobortis tincidunt et sapien rutrum ullamcorper egestas euismod. Sociosqu ultrices litora molestie quis blandit auctor. Lobortis duis condimentum sapien hac nostra massa. Libero nulla proin velit congue massa morbi risus. Praesent egestas inceptos tristique dignissim aptent suspendisse ante urna magna. Platea primis nisi netus magna volutpat. Velit efficitur morbi natoque tristique blandit tristique feugiat. Penatibus habitant senectus ligula vehicula cras est quam natoque placerat.
+
+Ultricies nec metus aenean morbi nunc, placerat praesent sociosqu. Est lacinia vivamus commodo tortor, et curabitur quis hac lacinia. Adipiscing ligula justo vestibulum sed felis maximus mauris. Mollis rutrum sed primis sed montes? Libero montes velit eget tincidunt curae. Lacinia integer rutrum ante efficitur augue phasellus semper. Purus rhoncus vulputate dignissim nunc aptent. At hendrerit cras maecenas accumsan odio vulputate.</p>
+
+                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Volutpat torquent maximus platea tincidunt aliquam inceptos curabitur donec. Eget faucibus congue hendrerit eros enim sit vel. Pellentesque dui lectus vulputate phasellus mollis faucibus, amet pellentesque. Mauris parturient dolor quisque orci volutpat sem laoreet. Quis orci ornare cubilia amet nec. Tristique porttitor dictum in vivamus placerat integer ac. Viverra sed faucibus leo dignissim, amet tincidunt cursus viverra. Nunc porttitor ex laoreet sem molestie platea varius enim fringilla.
+
+Venenatis et amet molestie ad platea interdum. Inceptos nec ligula diam orci rhoncus. Interdum leo rhoncus in nibh venenatis ex proin dictumst. Fames mi bibendum inceptos at mattis. Sollicitudin iaculis ex sapien suspendisse metus fusce est vestibulum. Class et per sapien hendrerit facilisi habitant lacus odio. Arcu dui litora luctus nec torquent ut commodo facilisi litora. Sagittis luctus lacinia malesuada mi consectetur; ipsum velit.
+
+Torquent massa egestas malesuada vulputate accumsan. Integer fringilla sollicitudin rutrum molestie neque enim. Aliquam venenatis fringilla elit vestibulum tellus. Torquent per leo blandit porta turpis. Efficitur metus adipiscing suscipit egestas cursus. Fusce libero magna justo vel ultricies.
+
+Parturient pretium luctus posuere; rutrum id sit. Platea fringilla primis aptent orci adipiscing nam; integer mollis scelerisque. Eget libero nec ligula gravida amet et pulvinar mollis. Euismod ad suspendisse egestas molestie nisi ridiculus maecenas per. Fringilla aenean primis et montes lobortis ipsum. Malesuada iaculis magna feugiat turpis tortor semper suscipit suscipit. Quam diam cubilia sed primis sollicitudin mi sapien quam. Fringilla ut curae porttitor nunc pellentesque pellentesque pulvinar vulputate.
+
+Leo sodales magnis cubilia arcu euismod fringilla efficitur ad. Lobortis tincidunt et sapien rutrum ullamcorper egestas euismod. Sociosqu ultrices litora molestie quis blandit auctor. Lobortis duis condimentum sapien hac nostra massa. Libero nulla proin velit congue massa morbi risus. Praesent egestas inceptos tristique dignissim aptent suspendisse ante urna magna. Platea primis nisi netus magna volutpat. Velit efficitur morbi natoque tristique blandit tristique feugiat. Penatibus habitant senectus ligula vehicula cras est quam natoque placerat.
+
+Ultricies nec metus aenean morbi nunc, placerat praesent sociosqu. Est lacinia vivamus commodo tortor, et curabitur quis hac lacinia. Adipiscing ligula justo vestibulum sed felis maximus mauris. Mollis rutrum sed primis sed montes? Libero montes velit eget tincidunt curae. Lacinia integer rutrum ante efficitur augue phasellus semper. Purus rhoncus vulputate dignissim nunc aptent. At hendrerit cras maecenas accumsan odio vulputate.</p>
+            </main>
         </div>
     );
 }
