7bacea324f4210f59e3e58095473b8bab339754f
Michael Blakeman <michael.l.blakeman@gmail.com>
2025-02-12 17:22:02 -0800

    Added POST route for submitting fuel data to local db. Also added dummy data for blk fuel sites and usr_main

--- fuel-kiosk/src/app/api/submit-fuel-data/route.js
+++
@@ -1,6 +0,0 @@
-export const revalidate = 0; // Forces regeneration on every request
-export async function POST(request) {
-    const formData = await request.json();
-    console.log('Received data:', formData);
-    return new Response(null, { status: 200 });
-  }
--- fuel-kiosk/test-utils/initDb.js
+++ fuel-kiosk/test-utils/initDb.js
@@ -3,10 +3,25 @@ const fs = require('fs');
 const prisma = new PrismaClient();
 
 const userData = require('./test-data/users.json');
+const bulkFuelData = require('./test-data/fuelData.json');
+const fuelSitesData = require('./test-data/fuelSites.json');
+const usrMainData = require('./test-data/usrMain.json');
 
 async function importData(){
     await prisma.user.createMany({
         data: userData,
+        skipDuplicates: true,
+    })
+    await prisma.uSR_MAIN.createMany({
+        data: usrMainData,
+        skipDuplicates: true,
+      })
+    await prisma.lOC_MAIN.createMany({
+        data: fuelSitesData,
+        skipDuplicates: true,
+    })
+    await prisma.fTK_bulkfuel.createMany({
+        data: bulkFuelData,
         skipDuplicates: true,
     })
 }
